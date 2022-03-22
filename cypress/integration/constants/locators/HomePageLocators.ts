0bc7ef14910ae7bd49df13fc735e5540dd96cd0c
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-03-22 10:31:25 -0400

    Removed unused constants

--- cypress/integration/constants/locators/HomePageLocators.ts
+++ cypress/integration/constants/locators/HomePageLocators.ts
@@ -4,8 +4,3 @@ export const HomePageLocators = {
     SIGN_OUT_BUTTON : "//a[contains(text(), 'Sign out')]",
     CONTACT_US_BUTTON : "//a[text()='Contact us']"
 } as const;
-
-export const homeButton = "//img/parent::a[@title='My Store']";
-export const AuthenticationButton = "//a[contains(text(), 'Sign in')]";
-export const signOutButton = "//a[contains(text(), 'Sign out')]";
-export const contactUsButton = "//a[text()='Contact us']";
