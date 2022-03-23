c3f783ca9045a47dbd517ba2099eef5656fffce0
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-03-23 13:39:52 -0400

    Added user utils file for setting up user info

--- cypress/integration/scenarios/authentication/login.spec.ts
+++ cypress/integration/scenarios/authentication/login.spec.ts
@@ -1,39 +1,32 @@
-import { CredentialConstants } from "../../constants/Constants";
 import RandomUserInfoUtils from "../../utils/RandomUserInfoUtils";
-import UserInfo from "../../utils/UserInfo";
+import { existingUser } from "../../utils/UserUtils";
 import AuthenticationWorkflow from "../../workflows/AuthenticationWorkflow";
 
 describe('Login', () => {
-
-    let existingUser : UserInfo;
-
-    before('set up user credentials', function() {
-        existingUser = new UserInfo(CredentialConstants.EXISTING_USER_EMAIL, CredentialConstants.EXISTING_USER_PASSWORD)
-    }); 
-
-    it('attempt login with an empty email', function() {
+ 
+    it('Attempt login with an empty email', function() {
         AuthenticationWorkflow.loginWithEmptyEmail();
     });
 
-    it('attempt login with a non-empty invalid email', function() {
+    it('Attempt login with a non-empty invalid email', function() {
         AuthenticationWorkflow.loginWithNonEmptyInvalidEmail(RandomUserInfoUtils.getInvalidEmail());
     });
 
-    it('attempt login with valid email but empty password', function() {
+    it('Attempt login with valid email but empty password', function() {
         AuthenticationWorkflow.loginWithValidEmailAndEmptyPassword(RandomUserInfoUtils.getValidEmail());
     });
 
-    it('attempt login with valid email but non-empty invalid password', function() {
+    it('Attempt login with valid email but non-empty invalid password', function() {
         AuthenticationWorkflow.loginWithValidEmailAndNonEmptyInvalidPassword(
             RandomUserInfoUtils.getValidEmail(), RandomUserInfoUtils.getInvalidPassword());
     });
 
-    it('attempt login with valid email but valid wrong password', function() {
+    it('Attempt login with valid email but valid wrong password', function() {
         AuthenticationWorkflow.loginWithValidEmailAndValidWrongPassword(
             existingUser.getEmail(), RandomUserInfoUtils.getValidPassword(existingUser.getPassword()));
     });
 
-    it('login successfully with existing user email and password', function() {
+    it('Login successfully with existing user email and password', function() {
         AuthenticationWorkflow.loginWithValidCredentials(
             existingUser.getEmail(), existingUser.getPassword());
     });
---
+++ cypress/integration/utils/UserUtils.ts
@@ -0,0 +1,4 @@
+import { CredentialConstants } from "../constants/Constants";
+import UserInfo from "./UserInfo";
+
+export let existingUser : UserInfo = new UserInfo(CredentialConstants.EXISTING_USER_EMAIL, CredentialConstants.EXISTING_USER_PASSWORD); 
