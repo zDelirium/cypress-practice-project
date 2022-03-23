987acd6563d36b4007cb566b455cca4786ec63cc
Gia-Khang Ernest Nguyen <ernest.nguyen@cgi.com>
2022-03-23 14:58:22 -0400

    Implemented simple builder for user containers

--- cypress/integration/utils/UserInfo.ts
+++ cypress/integration/utils/UserInfo.ts
@@ -1,4 +1,75 @@
+import { AppConstants } from "../constants/Constants";
+
 export default class UserInfo {
+
+    static UserBuilder = class {
+        initEmail: string;
+        initPassword: string;
+        initFirstName: string;
+        initLastName: string;
+        initAddress: string;
+        initCity: string;
+        initState:string = AppConstants.DROPDOWN_INVALID_VALUE;
+        initZipCode: string;
+        initCountry: string = AppConstants.DROPDOWN_INVALID_VALUE;
+        initMobilePhoneNumber: string;
+
+        build() : UserInfo {
+            return new UserInfo(this);
+        }
+
+        email(email: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initEmail = email;
+            return this;
+        }
+
+        password(password: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initPassword = password;
+            return this;
+        }
+
+        firstName(firstName: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initFirstName = firstName;
+            return this;
+        }
+
+        lastName(lastName: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initLastName = lastName;
+            return this;
+        }
+
+        address(address: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initAddress = address;
+            return this;
+        }
+
+        city(city: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initCity = city;
+            return this;
+        }
+
+        state(state: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initState = state;
+            return this;
+        }
+
+        zipCode(zipCode: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initZipCode = zipCode;
+            return this;
+        }
+
+        country(country: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initCountry = country;
+            return this;
+        }
+
+        mobilePhoneNumber(mobilePhoneNumber: string) : typeof UserInfo.UserBuilder.prototype {
+            this.initMobilePhoneNumber = mobilePhoneNumber;
+            return this;
+        }
+
+    };
+
     private email: string;
     private password: string;
     private firstName: string;
@@ -10,10 +81,17 @@ export default class UserInfo {
     private country: string;
     private mobilePhoneNumber: string;
 
-    // TODO implement builder pattern
-    constructor(email: string, password: string) {
-        this.email = email;
-        this.password = password;
+    constructor(builder: typeof UserInfo.UserBuilder.prototype) {
+        this.email = builder.initEmail;
+        this.password = builder.initPassword;
+        this.firstName = builder.initFirstName;
+        this.lastName = builder.initLastName;
+        this.address = builder.initAddress;
+        this.city = builder.initCity;
+        this.state = builder.initState
+        this.zipCode = builder.initZipCode;
+        this.country = builder.initCountry;
+        this.mobilePhoneNumber = builder.initMobilePhoneNumber;
     }
 
     // Getters
--- cypress/integration/utils/UserUtils.ts
+++ cypress/integration/utils/UserUtils.ts
@@ -1,4 +1,7 @@
 import { CredentialConstants } from "../constants/Constants";
 import UserInfo from "./UserInfo";
 
-export let existingUser : UserInfo = new UserInfo(CredentialConstants.EXISTING_USER_EMAIL, CredentialConstants.EXISTING_USER_PASSWORD); 
+export let existingUser : UserInfo = new UserInfo.UserBuilder()
+    .email(CredentialConstants.EXISTING_USER_EMAIL)
+    .password(CredentialConstants.EXISTING_USER_PASSWORD)
+    .build();
